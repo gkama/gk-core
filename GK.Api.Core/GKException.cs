c8996bae670b6cee9b9c62b609bab887b4ab42b3
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-22 17:16:27 -0400

    add gk exception

---
+++ GK.Api.Core/GKException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Text.Json;
+
+namespace GK.Api.Core
+{
+    public class GKException : ApplicationException
+    {
+        public int StatusCode { get; set; }
+        public string ContentType { get; set; }
+
+        public GKException(int StatusCode)
+        {
+            this.StatusCode = StatusCode;
+        }
+
+        public GKException(string Message) : base(Message)
+        {
+            this.StatusCode = (int)HttpStatusCode.InternalServerError;
+        }
+
+        public GKException(int StatusCode, string Message) : base(Message)
+        {
+            this.StatusCode = StatusCode;
+        }
+
+        public GKException(HttpStatusCode StatusCode, string Message) : base(Message)
+        {
+            this.StatusCode = (int)StatusCode;
+        }
+
+        public GKException(int StatusCode, Exception Inner) : this(StatusCode, Inner.ToString()) { }
+        public GKException(HttpStatusCode StatusCode, Exception Inner) : this(StatusCode, Inner.ToString()) { }
+        public GKException(int StatusCode, JsonElement ErrorObject) : this(StatusCode, ErrorObject.ToString()) { this.ContentType = @"application/problem+json"; }
+    }
+}
