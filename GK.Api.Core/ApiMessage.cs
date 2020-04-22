a9e7665146af37c2414d18ef85fc217cc68e2e79
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-22 14:05:56 -0400

    add api message

---
+++ GK.Api.Core/ApiMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GK.Api.Core
+{
+    /// <summary>
+    /// Represents general information about the results of an operation.
+    /// </summary>
+    public class ApiMessage
+    {
+        public string Message { get; set; }
+
+        public ApiMessage() { }
+        public ApiMessage(string message) { Message = message; }
+    }
+}
--- GK.Api.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace GK.Api.Core
-{
-    public class Class1
-    {
-    }
-}
