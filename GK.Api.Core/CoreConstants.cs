e4b125432173f5ed95011ae3e60a4d89ead2a488
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-22 16:23:43 -0400

    add core constants

---
+++ GK.Api.Core/CoreConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GK.Api.Core
+{
+    internal static class CoreConstants
+    {
+        public const string JsonContentType = "application/json";
+        public const string RequestIdHttpHeader = "X-RequestId";
+        public const string DefaultLoggerCategory = "GK API Logger";
+
+        internal const string AboutPagePrefix = "about";
+        internal const string HealthCheckRoutePrefix = "health";
+        internal const string SwaggerRoutePrefix = "docs";
+        internal const string SwaggerJsonEndpoint = "swagger.json";
+    }
+}
