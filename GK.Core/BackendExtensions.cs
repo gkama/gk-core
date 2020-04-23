d81303da7e92ec514fa91c6382ae75b3d24955ec
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-23 16:09:18 -0400

    add backend extensions

---
+++ GK.Core/BackendExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+using JetBrains.Annotations;
+
+namespace GK.Core
+{
+    public static class BackendExtensions
+    {
+        /// <summary>
+        /// Returns a full name composed of the contact's individual name components, suitable for display or correspondence.
+        /// </summary>
+        [NotNull]
+        public static string GetFullName(this IContact contact)
+        {
+            if (contact == null)
+                return string.Empty;
+
+            IEnumerable<string> GetNonEmptyItems(params string[] items)
+            {
+                return items.Where(x => !string.IsNullOrWhiteSpace(x));
+            }
+
+            return string.Join(" ", GetNonEmptyItems(contact.Prefix, contact.FirstName, contact.MiddleName,
+                contact.LastName, contact.Suffix));
+        }
+    }
+}
