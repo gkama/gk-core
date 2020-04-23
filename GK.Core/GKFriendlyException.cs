299069adebcd6787706a588bb7d24a30db235354
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-23 15:50:01 -0400

    add gk friendly exception

---
+++ GK.Core/GKFriendlyException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GK.Core
+{
+    public class GKFriendlyException : GKException
+    {
+        public GKFriendlyException()
+        { }
+
+        public GKFriendlyException(string message) : base(message)
+        { }
+
+        public GKFriendlyException(string message, Exception inner) : base(message, inner)
+        { }
+    }
+}
