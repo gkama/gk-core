7cbb8d7ee098c63c42da34e3643b29afe7d46aee
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-23 09:50:15 -0400

    add public key id interface

---
+++ GK.Core/IPublicKeyId.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GK.Core
+{
+    /// <summary>
+    /// An entity which uses an integer id and GUID public key.
+    /// </summary>
+    public interface IPublicKeyId
+    {
+        int Id { get; set; }
+        Guid PublicKey { get; set; }
+    }
+}
