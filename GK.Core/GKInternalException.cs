d529114f0851a1800fc1f192c2cffb7a7b3b7735
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-23 15:56:01 -0400

    add gk internal exception

---
+++ GK.Core/GKInternalException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GK.Core
+{
+    /// <summary>
+    /// This exception is used to capture internal errors which should not be displayed/returned to the caller.
+    /// A general error message will be returned instead.
+    /// </summary>
+    public class GKInternalException : GKException
+    {
+        public GKInternalException()
+        { }
+
+        public GKInternalException(string message) : base(message)
+        { }
+
+        public GKInternalException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
