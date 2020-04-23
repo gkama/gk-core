3ddcd326be090139e6cd04c6c1e0369259450254
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-23 16:09:09 -0400

    add contact interface

---
+++ GK.Core/IContact.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GK.Core
+{
+    /// <summary>
+    /// Describes a type which has name and contact information.
+    /// </summary>
+    public interface IContact
+    {
+        /// <summary>
+        /// The email address of this contact.
+        /// </summary>
+        string Email { get; set; }
+
+        string CellPhone { get; set; }
+
+        string FirstName { get; set; }
+        string MiddleName { get; set; }
+        string LastName { get; set; }
+
+        string Prefix { get; set; }
+        string Suffix { get; set; }
+
+        string FullName { get; }
+        string DataName { get; set; }
+    }
+}
