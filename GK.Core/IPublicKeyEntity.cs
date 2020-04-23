e36a032d960f79e89891d6381406d16311a36501
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-23 09:56:58 -0400

    add public key entity interface

---
+++ GK.Core/IPublicKeyEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GK.Core
+{
+    public interface IPublicKeyEntity
+    {
+        /// <summary>
+        /// Used to uniquely identify this entity within its partition.
+        /// </summary>
+        Guid PublicKey { get; set; }
+
+        /// <summary>
+        /// Used by clients to uniquely identify an entity.
+        /// </summary>
+        Guid ClientGuid { get; set; }
+
+        /// <summary>
+        /// A GUID representing the workspace to which this entity belongs.
+        /// </summary>
+        Guid WorkspaceGuid { get; set; }
+    }
+}
