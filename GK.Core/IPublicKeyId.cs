4aacfd0b97f8f7572a0128d9780138eb90fb6620
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-23 09:57:37 -0400

    update summary of fields

--- GK.Core/IPublicKeyId.cs
+++ GK.Core/IPublicKeyId.cs
@@ -7,7 +7,14 @@ namespace GK.Core
     /// </summary>
     public interface IPublicKeyId
     {
+        /// <summary>
+        /// Used to uniquely identify this entity within a database.
+        /// </summary>
         int Id { get; set; }
+
+        /// <summary>
+        /// Used to uniquely identify this entity within its partition.
+        /// </summary>
         Guid PublicKey { get; set; }
     }
 }
