887a91067466d80a97e35f769ab79262921a8cfa
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-24 10:28:51 -0400

    add config sections const values

---
+++ GK.Api.Core/ConfigSections.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GK.Api.Core
+{
+    /// <summary>
+    /// Standard config section names for common settings.
+    /// </summary>
+    public static class ConfigSections
+    {
+        public const string ConnectionStrings = nameof(ConnectionStrings);
+        public const string SQLServer = nameof(SQLServer);
+        public const string MySQL = nameof(MySQL);
+        public const string PostgreSQL = nameof(PostgreSQL);
+        public const string CosmosDB = nameof(CosmosDB);
+
+        public const string KeyVault = nameof(KeyVault);
+
+        public const string Redis = nameof(Redis);
+
+        public const string FeatureFlags = nameof(FeatureFlags);
+    }
+}
