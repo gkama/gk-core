853713a2f6f9f822442f15d8e3b9a2a88bb9e1ba
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-24 10:30:08 -0400

    add azure key vault options

---
+++ GK.Api.Core/AzureKeyVaultOptions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GK.Api.Core
+{
+    /// <summary>
+    /// Options for reading secrets from Azure Key Vault.
+    /// </summary>
+    public class AzureKeyVaultOptions
+    {
+        /// <summary>
+        /// The URL of the Azure Key Vault.
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// A unique key which identifies the application connecting to the Key Vault.
+        /// </summary>
+        public string ApplicationId { get; set; }
+
+        /// <summary>
+        /// The Client Secret / Password used to connect to the Azure Key Vault.
+        /// </summary>
+        public string Secret { get; set; }
+
+        public override string ToString() => $"{Url}; {ApplicationId}";
+    }
+}
