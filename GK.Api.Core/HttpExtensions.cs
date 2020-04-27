7f21d9bfbaa7a3097af658d7c39a13e83946aeec
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-27 09:52:44 -0400

    add http extension methods and add get request id

---
+++ GK.Api.Core/HttpExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using JetBrains.Annotations;
+
+namespace GK.Api.Core
+{
+    public static class HttpExtensions
+    {
+        /// <summary>
+        /// If a <see cref="HttpContext.TraceIdentifier"/> has already been set by the framework, returns it; otherwise generates
+        /// a new one then sets and returns it.
+        /// </summary>
+        public static string GetRequestId([CanBeNull] this HttpContext httpContext)
+        {
+            if (!string.IsNullOrEmpty(httpContext?.TraceIdentifier))
+                return httpContext.TraceIdentifier;
+
+            var id = Guid.NewGuid().ToString();
+
+            if (httpContext != null)
+                httpContext.TraceIdentifier = id;
+
+            return id;
+        }
+    }
+}
