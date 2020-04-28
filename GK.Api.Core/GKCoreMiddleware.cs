4c217ccfd6167541f016b0e01b91fcf77179ac7e
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-28 16:56:23 -0400

    add soace between libraries

--- GK.Api.Core/GKCoreMiddleware.cs
+++ GK.Api.Core/GKCoreMiddleware.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
