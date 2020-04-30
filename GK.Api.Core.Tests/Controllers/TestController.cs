600e118726a964187aef57984f6269cc51e4b28b
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-30 11:15:23 -0400

    initial integration test project

---
+++ GK.Api.Core.Tests/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GK.Api.Core.Tests.Controllers
+{
+    [ApiController]
+    [Route("test")]
+    public class TestController : ControllerBase
+    {
+        
+    }
+}
