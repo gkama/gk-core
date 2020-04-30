bd93fdb19dd2563324bbe24bd5a60e34f1424c97
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-30 11:10:21 -0400

    add authorize with xapi filter / attribute

---
+++ GK.Api.Core/AuthorizeXApiFilter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+using GK.Core;
+
+namespace GK.Api.Core
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class AuthorizeXApiAttribute : TypeFilterAttribute
+    {
+        public AuthorizeXApiAttribute()
+            : base(typeof(AuthorizeXApiFilter))
+        {
+        }
+    }
+
+    public class AuthorizeXApiFilter : IAuthorizationFilter
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly IConfiguration _configuration;
+
+        public AuthorizeXApiFilter(IWebHostEnvironment env, IConfiguration configuration)
+        {
+            _env = env ?? throw new GKFriendlyException(HttpStatusCode.InternalServerError, nameof(env));
+            _configuration = configuration ?? throw new GKFriendlyException(HttpStatusCode.InternalServerError, nameof(configuration));
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (_env.IsDevelopment())
+                return;
+
+            string authHeader = context.HttpContext.Request.Headers["x-api-key"];
+
+            if (authHeader != null
+                && authHeader.StartsWith("x-api-key"))
+            {
+                var encodedwriteKey = authHeader.Substring("x-api-key ".Length).Trim();
+                var writeKey = Encoding.UTF8.GetString(Convert.FromBase64String(encodedwriteKey)).TrimEnd(':');
+
+                if (writeKey == _configuration["XApiKey"])
+                    return;
+            }
+
+            context.HttpContext.Response.Headers["WWW-Authenticate"] = "x-api-key";
+            context.Result = new UnauthorizedResult();
+        }
+    }
+}
