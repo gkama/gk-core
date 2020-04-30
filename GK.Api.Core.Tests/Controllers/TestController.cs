31163116ab02e2106017edfc7d133384fac7fb20
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-30 11:21:28 -0400

    update test controller

--- GK.Api.Core.Tests/Controllers/TestController.cs
+++ GK.Api.Core.Tests/Controllers/TestController.cs
@@ -6,12 +6,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
+using GK.Api.Core;
+
 namespace GK.Api.Core.Tests.Controllers
 {
     [ApiController]
     [Route("test")]
     public class TestController : ControllerBase
     {
-        
+        [AuthorizeXApi]
+        [HttpGet]
+        [Route("string")]
+        public string GetString()
+        {
+            return "here";
+        }
     }
 }
